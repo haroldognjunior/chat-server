63ac840e83797754bd5b37b434d201394a883f83
Júnior Nogueira <junior.nogueira@getpulpo.com>
2023-10-07 02:23:18 -0600

    feature: chat server

--- index.js
+++ index.js
@@ -13,14 +13,24 @@ const io = new SocketServer(server, {
   },
 });
 
+// Configuración de Express
 app.use(cors());
 app.use(morgan("dev"));
 
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+// Manejo de conexiones de Socket.io
 io.on("connection", socket => {
   socket.on("message", msg => {
     socket.broadcast.emit("message", { body: msg.body, user: msg.user });
   });
 });
 
-server.listen(PORT);
-console.log(`Server started on port: ${PORT}`);
+// Inicio del servidor
+server.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
